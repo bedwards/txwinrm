bedwards/txwinrm

commit e5ca422282ed2631c05175b88d63e5ffa9b4a36a

Mandatory timeout for DeferredSemaphore(1)
Cases were found where operations running under the session's
DeferredSemaphore(1) would encounter network timeouts during their
connection. This would result in the semaphore being permanently
acquired, and prevent all further operations requiring the semaphore
from running until the process was restarted.

This change adds a mandatory timeout to all operations running under the
session's semaphore to prevent this to make sure that the semaphore
never gets into a permanently-acquired state.

Refs ZPS-1765.

txwinrm/WinRMClient.py
@@ -66,6 +66,7 @@ from .enumerate import (
     _MAX_REQUESTS_PER_ENUMERATION
 )
 from .SessionManager import SESSION_MANAGER, Session
+from .twisted_utils import add_timeout
 kerberos = None
 LOG = logging.getLogger('winrm')
 
@@ -98,6 +99,13 @@ class WinRMSession(Session):
         self.sem = DeferredSemaphore(1)
 
         self._refresh_dc = None
+
+    def semrun(self, fn, *args, **kwargs):
+        """Run fn(*args, **kwargs) under a DeferredSemaphore with a timeout."""
+        return self.sem.run(
+            add_timeout,
+            fn(*args, **kwargs),
+            self._conn_info.timeout)
 
     def is_kerberos(self):
         return self._conn_info.auth_type == 'kerberos'
@@ -370,8 +378,9 @@ class SingleCommandClient(WinRMClient):
         self.ps_script = ps_script
         yield self.init_connection()
         try:
-            cmd_response = yield self._session().sem.run(self.run_single_command,
-                                                         command_line)
+            cmd_response = yield self._session().semrun(
+                self.run_single_command,
+                command_line)
         except Exception:
             yield self.close_connection()
             raise
@@ -549,9 +558,10 @@ class EnumerateClient(WinRMClient):
         yield self.init_connection()
         for enum_info in enum_infos:
             try:
-                items[enum_info] = yield self._session().sem.run(self.enumerate,
-                                                                 enum_info.wql,
-                                                                 enum_info.resource_uri)
+                items[enum_info] = yield self._session().semrun(
+                    self.enumerate,
+                    enum_info.wql,
+                    enum_info.resource_uri)
             except (UnauthorizedError, ForbiddenError):
                 # Fail the collection for general errors.
                 raise

txwinrm/twisted_utils.py
@@ -1,0 +1,45 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2016, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+from twisted.internet import defer, error, reactor
+from twisted.python import failure
+
+
+def add_timeout(deferred, seconds, exception_class=error.TimeoutError):
+    """Return new Deferred that will errback exception_class after seconds."""
+    deferred_with_timeout = defer.Deferred()
+
+    def fire_timeout():
+        deferred.cancel()
+
+        if not deferred_with_timeout.called:
+            deferred_with_timeout.errback(exception_class())
+
+    delayed_timeout = reactor.callLater(seconds, fire_timeout)
+
+    def handle_result(result):
+        is_failure = isinstance(result, failure.Failure)
+        is_cancelled = is_failure and isinstance(result.value, defer.CancelledError)
+
+        if delayed_timeout.active():
+            # Cancel the timeout since a result came before it fired.
+            delayed_timeout.cancel()
+        elif is_cancelled:
+            # Don't propagate cancellations that we caused.
+            return
+
+        # Propagate remaining results.
+        if is_failure:
+            deferred_with_timeout.errback(result)
+        else:
+            deferred_with_timeout.callback(result)
+
+    deferred.addBoth(handle_result)
+
+    return deferred_with_timeout

commit fd449eaf707b24b12df76ba07ae2a092a2795f80

Fix 'kerberos' referenced before assignment
DeferredSemaphore.run(fn, *args, **kwargs) has the method signature it
does because it defers the creation of the fn(*args, **kwargs) deferred
until the semaphore can be acquired. My change to wrap a timeout around
all functions run under the semaphore in e5ca422 broke this by causing
fn(*args, **kwargs) to be executed before the semaphore could be
acquired.

This change restores the execution of fn(*args, **kwargs) to after the
semaphore is acquired, and thus fixes the resulting 'kerberos'
referenced before assignment problems it caused in the Windows ZenPack.

Refs ZPS-1795.

txwinrm/WinRMClient.py
@@ -66,7 +66,7 @@ from .enumerate import (
     _MAX_REQUESTS_PER_ENUMERATION
 )
 from .SessionManager import SESSION_MANAGER, Session
-from .twisted_utils import add_timeout
+from .twisted_utils import with_timeout
 kerberos = None
 LOG = logging.getLogger('winrm')
 
@@ -103,9 +103,11 @@ class WinRMSession(Session):
     def semrun(self, fn, *args, **kwargs):
         """Run fn(*args, **kwargs) under a DeferredSemaphore with a timeout."""
         return self.sem.run(
-            add_timeout,
-            fn(*args, **kwargs),
-            self._conn_info.timeout)
+            with_timeout,
+            fn=fn,
+            args=args,
+            kwargs=kwargs,
+            seconds=self._conn_info.timeout)
 
     def is_kerberos(self):
         return self._conn_info.auth_type == 'kerberos'

txwinrm/twisted_utils.py
@@ -11,6 +11,14 @@ from twisted.internet import defer, error, reactor
 from twisted.python import failure
 
 
+def with_timeout(fn, args=None, kwargs=None, seconds=None, exception_class=error.TimeoutError):
+    """Execute asynchronous function fn(*args, **kwargs) with a timeout."""
+    return add_timeout(
+        deferred=fn(*args, **kwargs),
+        seconds=seconds,
+        exception_class=exception_class)
+
+
 def add_timeout(deferred, seconds, exception_class=error.TimeoutError):
     """Return new Deferred that will errback exception_class after seconds."""
     deferred_with_timeout = defer.Deferred()