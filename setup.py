2181ab7f03252ffdd8d81653ab495055b781fbab
Dave Bouchillon <dbouchillon@zenoss.com>
2017-07-25 11:39:09 -0500

    Begin hotfix: Version 1.2.22 -> 1.2.23

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.2.22',
+    version='1.2.23',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
