5bfbdaa22d678c6df189fc3718c397d86fa8f50f
Dave Bouchillon <dbouchillon@zenoss.com>
2018-02-02 16:46:03 -0600

    Begin hotfix: Version 1.3.8 -> 1.3.9

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.3.8',
+    version='1.3.9',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
